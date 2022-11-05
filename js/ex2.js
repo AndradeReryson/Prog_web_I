a9133b28d53eb5c1e3586326247dd429b75eb2be
AndradeReryson <reryson.andrade@fatec.sp.gov.br>
2022-11-05 11:21:21 -0300

    aula-05.11-2

---
+++ js/ex2.js
@@ -0,0 +1,28 @@
+function validar(){
+    let CPF = cadastro.cpf.value
+    let senha = cadastro.senha.value
+    let confirm = cadastro.conf_senha.value
+
+    confirm = String(confirm)
+    senha = String(senha)
+
+    if(CPF.length > 11 || CPF.length < 11){
+        alert("CPF de tamanho invalido")
+        return false
+    }
+
+    if(isNaN(CPF)){
+        alert("informe apenas numeros no CPF")
+        cadastro.cpf.focus()
+        return false
+    }
+
+    if(senha != confirm){
+        alert("Senhas estão diferentes")
+        cadastro.senha.value = null
+        cadastro.conf_senha.focus()
+        return false
+    }
+
+    document.cadastro.submit()
+}
